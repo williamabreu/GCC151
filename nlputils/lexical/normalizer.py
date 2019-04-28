4df6a2a7e184af6def2ab821b364ef796f38824d
williamabreu <williamsantos_00@hotmail.com>
2019-04-28 11:17:21 -0300

    Novas funções testadas em normalizer
    - remove_ponctuations
    - remove_accents
    - remove_stopwords
    - to_lowercase

--- nlputils/lexical/normalizer.py
+++ nlputils/lexical/normalizer.py
@@ -2,26 +2,31 @@ import nltk
 import unidecode
 import string
 
+
 class Normalizer:
     def __init__(self):
-        self.sent_tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle')
+        self.sent_tokenizer = nltk.data.load(
+            'tokenizers/punkt/portuguese.pickle')
         self.stemmer = nltk.stem.RSLPStemmer()
-    
-    def remove_accents(self, text):
-        return unidecode.unidecode(text)
-    
+
     def remove_ponctuations(self, text):
         return text.translate(str.maketrans('', '', string.punctuation))
-    
-    def tokenize_sentences(self, text):
-        return self.sent_tokenizer.tokenize(text)
-    
-    def tokenize_words(self, text):
-        return nltk.tokenize.word_tokenize(text)
-    
-    def lemmatize(self, text):
-        return text
-    
+
+    def remove_accents(self, text):
+        return unidecode.unidecode(text)
+
+    def remove_stopwords(self, tokens):
+        stopwords = nltk.corpus.stopwords.words('portuguese')
+        return [word for word in tokens if word not in stopwords]
+
+    def to_lowercase(self, text):
+        return text.lower()
+
     def stemmize(self, tokens):
         return [self.stemmer.stem(word) for word in tokens]
-    
+
+    def tokenize_sentences(self, text):
+        return self.sent_tokenizer.tokenize(text)
+
+    def tokenize_words(self, text):
+        return nltk.tokenize.word_tokenize(text)
