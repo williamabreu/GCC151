36163c507d314251e77ca175de9d6a07d85cc5c6
williamabreu <williamsantos_00@hotmail.com>
2019-05-23 19:55:15 -0300

    Adicionada estrutura da classe Tagger

--- nlputils/morphosyntax/tagger.py
+++ nlputils/morphosyntax/tagger.py
@@ -1,2 +1,19 @@
 class Tagger:
-    pass
+    """
+    Faz etiquetagem morfossintática
+    """
+
+    def __init__(self):
+        """
+        Instancia o objeto configurado para tratar língua portuguesa.
+        """
+        pass
+
+    def tag(self, string: str) -> list:
+        """
+        Faz etiquetagem de uma string qualquer.
+
+        :param string: String qualquer em português
+        :return: Lista de tuplas no formato ('token', 'morphosyntax_tag')
+        """
+        pass
