9778c4d1907f62fa60b346ae4b923c6799fa83ee
williamabreu <williamsantos_00@hotmail.com>
2019-05-23 19:55:29 -0300

    Adicionada estrutura da classe Parser

--- nlputils/syntax/parser.py
+++ nlputils/syntax/parser.py
@@ -1,2 +1,28 @@
 class Parser:
-    pass
+    """
+    Faz análise sintática de sentenças
+    """
+
+    def __init__(self):
+        """
+        Instancia o objeto configurado para tratar língua portuguesa.
+        """
+        pass
+
+    def parse(self, sentence: str) -> list:
+        """
+        Faz parsing de uma sentença em português.
+
+        :param sentence: Sentença qualquer em português
+        :return: Lista de tuplas no formato ('token', 'syntax_role', 'head')
+        """
+        pass
+
+    def get_SVO(self, sentence: str) -> list:
+        """
+        Estrutura uma sentença em Sujeito-Verbo-Objeto
+
+        :param sentence: Sentença qualquer em português
+        :return: Lista de tuplas no formato ('subject', 'verb', 'object')
+        """
+        pass
